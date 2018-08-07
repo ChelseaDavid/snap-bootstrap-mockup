d50aacb1e457e68514574038533f035f0a9c5221
Chelsea David <cryan17@cnm.edu>
2018-08-07 10:19:18 -0600

    completed navbar and changed icon

--- snap-bootstrap-mockup.php
+++ snap-bootstrap-mockup.php
@@ -8,11 +8,11 @@
 		<title>Bootstrap Mockup</title>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<nav class="navbar navbar-expand-lg navbar-light bg-primary text-white">
 			<!-- Just an image -->
-			<nav class="navbar navbar-light bg-light">
-				<a class="navbar-brand" href="#">
-					<img src="images/bootstrap-mockup.png" alt="dog & cat icon">
+			<nav class="navbar">
+				<a class="navbar-brand" href="https://www.pottermore.com/">
+					<img src="images/hogwarts-crest.png" width="100" height="100" alt="crest">
 				</a>
 			</nav>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,23 +21,25 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
 					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						<a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
+						<a class="nav-link" href="https://www.pottermore.com">Link</a>
 					</li>
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Dropdown
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
+							<a class="dropdown-item" href="https://www.pottermore.com/">Action</a>
+							<a class="dropdown-item" href="https://www.pottermore.com/">Another action</a>
 							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Something else here</a>
+							<a class="dropdown-item" href="https://www.pottermore.com/">Something else here</a>
 						</div>
+					</li>
 				</ul>
 			</div>
 		</nav>
+
 	</body>
 </html>
